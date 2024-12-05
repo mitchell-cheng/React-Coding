22364ce5003facb4c4e442da13004f139993112a
Mitchell Cheng <chengbotier1@gmail.com>
2024-12-05 23:09:28 +0800

    fix

--- UI-Components/Contact Form/script.js
+++ UI-Components/Contact Form/script.js
@@ -1,36 +1,36 @@
-const SUBMIT_URL = '';
+const SUBMIT_URL = "";
 
 export default async function submitForm(event) {
   event.preventDefault();
   const form = event.target;
-  
+
   try {
     if (form.action !== SUBMIT_URL) {
-      alert('Incorrect form action value');
+      alert("Incorrect form action value");
       return;
     }
 
-    if (form.method.toLowerCase() !== 'post') {
-      alert('Incorrect form method value');
+    if (form.method.toLowerCase() !== "post") {
+      alert("Incorrect form method value");
       return;
     }
-    
+
     const formData = new FormData(form);
     const response = await fetch(SUBMIT_URL, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        "Content-Type": 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-     		name: formData.get('name'),
-        email: formData.get('email'),
-        message: formData.get('message'),
-      });
+        name: formData.get("name"),
+        email: formData.get("email"),
+        message: formData.get("message"),
+      }),
     });
-    
+
     const text = await response.text();
     alert(text);
   } catch (_) {
-    alert('Error submitting form!');
+    alert("Error submitting form!");
   }
 }
